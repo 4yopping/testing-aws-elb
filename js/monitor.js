ed00f5ed20a52bbc3fa83f40e2b4e1e8df3ece5c
Marco Godínez <markotom@gmail.com>
2016-02-18 17:47:12 -0600

    Fix error when trying to get autoscaling instances

--- js/monitor.js
+++ js/monitor.js
@@ -28,38 +28,54 @@ let describeInstances = function () {
     if (err) return console.error(err)
     assert(res.Reservations.length, `Not found instances with ${config.storeId} security group`)
 
-    let instances = res.Reservations[0].Instances
+    let instances = []
+    res.Reservations.forEach((r) => {
+      instances = instances.concat(r.Instances)
+    })
+
+    let instances_ids = $('.instance').toArray()
+    if (instances_ids.length) {
+      let currentInstances = instances.map((i) => i.InstanceId)
+      instances_ids.forEach((i) => {
+        let id = $(i).attr('id')
+        if (currentInstances.indexOf(id) < 0) {
+          $(i).remove()
+        }
+      })
+    }
 
     instances.forEach((instance) => {
       let instanceElement = $(`#${instance.InstanceId}`)
 
+      if (instanceElement.length) {
+        instanceElement.find('.instance-address').html(instance.PublicIpAddress)
+        instanceElement.find('.instance-state').html(instance.State.Name)
+      } else {
+        instanceElement = $(`
+          <div id="${instance.InstanceId}" class="instance">
+            <div>
+              <strong>Address</strong>:
+              <span class="instance-address">${instance.PublicIpAddress}</span>
+            </div>
+            <div>
+              <strong>State</strong>:
+              <span class="instance-state">${instance.State.Name}</span>
+            </div>
+            <div>
+              <strong>Status Code</strong>:
+              <span class="instance-status">0</span>
+            </div>
+          </div>
+        `)
+
+        wrapper.append(instanceElement)
+      }
+
       $.ajax({
         url: `http://${instance.PublicIpAddress}/`,
+        timeout: 5000,
         complete: function (xhr) {
-          if (instanceElement.length) {
-            instanceElement.find('.instance-address').html(instance.PublicIpAddress)
-            instanceElement.find('.instance-state').html(instance.State.Name)
-            instanceElement.find('.instance-status').html(xhr.status)
-          } else {
-            instanceElement = $(`
-              <div id="${instance.InstanceId}" class="instance">
-                <div>
-                  <strong>Address</strong>:
-                  <span class="instance-address">${instance.PublicIpAddress}</span>
-                </div>
-                <div>
-                  <strong>State</strong>:
-                  <span class="instance-state">${instance.State.Name}</span>
-                </div>
-                <div>
-                  <strong>Status Code</strong>:
-                  <span class="instance-status">${xhr.status}</span>
-                </div>
-              </div>
-            `)
-
-            wrapper.append(instanceElement)
-          }
+          instanceElement.find('.instance-status').html(xhr.status)
 
           if (/^2/.test(xhr.status)) {
             instanceElement.removeClass('out-of-service').addClass('in-service')
@@ -80,6 +96,7 @@ let testLoadBalancer = function () {
 
   $.ajax({
     url: config.elb,
+    timeout: 5000,
     complete: function (xhr) {
       if (/^2/.test(xhr.status)) {
         elbElement.removeClass('out-of-service').addClass('in-service')
